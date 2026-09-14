156d4410082876f89718d4df6e03d93265877067
abhiram141902-create <abhiram141902@gmail.com>
2026-09-14 14:20:24 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8080;
+const DIR = __dirname;
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.png': 'image/png',
+    '.webp': 'image/webp',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml'
+};
+
+const server = http.createServer((req, res) => {
+    let filePath = req.url === '/' ? '/index.html' : req.url;
+    filePath = path.join(DIR, filePath);
+    const ext = path.extname(filePath);
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not found');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': mimeTypes[ext] || 'text/html' });
+        res.end(data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log('Server running at http://localhost:' + PORT);
+});
